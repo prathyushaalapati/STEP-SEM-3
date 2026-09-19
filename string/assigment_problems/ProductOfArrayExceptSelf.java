9a93230e12c11ee43eb8fbf5ad3439481a63f260
pratyushaalapati <na0752@srmist.edu.in>
2026-09-19 23:22:09 +0530

    Add Product of Array Except Self

---
+++ string/assigment_problems/ProductOfArrayExceptSelf.java
@@ -0,0 +1,37 @@
+import java.util.Arrays;
+
+public class ProductOfArrayExceptSelf {
+
+    public static int[] productExceptSelf(int[] nums) {
+
+        int n = nums.length;
+        int[] answer = new int[n];
+
+        // Forward pass: products of elements on the left
+        int leftProduct = 1;
+
+        for (int i = 0; i < n; i++) {
+            answer[i] = leftProduct;
+            leftProduct *= nums[i];
+        }
+
+        // Backward pass: multiply products of elements on the right
+        int rightProduct = 1;
+
+        for (int i = n - 1; i >= 0; i--) {
+            answer[i] *= rightProduct;
+            rightProduct *= nums[i];
+        }
+
+        return answer;
+    }
+
+    public static void main(String[] args) {
+
+        int[] nums = {1, 2, 3, 4};
+
+        int[] result = productExceptSelf(nums);
+
+        System.out.println(Arrays.toString(result));
+    }
+}
